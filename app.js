1de1e226770c02d0043d5030f512a7c2c1959cc9
Fahad-Rafiq06 <73123765+Fahad-Rafiq06@users.noreply.github.com>
2022-06-21 11:57:55 +0500

    updated
    updated with complete functionality of Vowels and Consonants

--- app.js
+++ app.js
@@ -2,62 +2,29 @@ var vowels = [];
 
 var normals = [];
 
-            function addwords() {
-                var cont = document.getElementById('input').value;
-                var value = true;
-
-
-                for (var i = 0; i < cont.length; i++) {
-                    if (cont[i] == "a" || cont[i] == "e" || cont[i] == "i" || cont[i] == "o" || cont[i] == "u") {
-                        value = false;
-                        
-
-                        if (value == false) {
-                            vowels.push(cont)
-                            document.getElementById('a').innerHTML += "<li>" + cont + "</li>";
-                            
-                        }
-                        else {
-                            // value = true;
-                            normals.push(cont)
-                            document.getElementById('b').innerHTML += "<li>" + cont + "</li>";
-                            console.log(normals)
-                            
-                        };
-                        break;
-                    }
-
-                }
-            }
-
-
-
-// var vowels = [];
-
-// var normals = [];
-
-// var value = true;
-
-// function addwords() {
-//     var userValue = document.getElementById("store").value;
-//     for (var i = 0; i <= userValue.length; i++) {
-//         console.log(i)
-//         if (userValue[i] == "a" || userValue[i] == "e" || userValue[i] == "i" || userValue[i] == "o" || userValue[i] == "u") {
-//             vowels.push(userValue);
-//             value = false;
-//             console.log("vowels wins", vowels)
-//             document.getElementById("vowels").innerText += "<li>" + userValue + "</li>"
-//             break;
-//         }
-//         else {
-//             normals.push(userValue);
-//             value = true;
-//             document.getElementById("normals").innerText += "<li>" + userValue + "</li>"
-//             console.log("normal wins", normals);
-//             break;
-
-//         }
-
-
-//     }
-// }
+function addwords() {
+    var cont = document.getElementById('input').value;
+    cont = cont.toLowerCase();
+    var value = true;
+    if(cont == "" || cont == " "){
+        alert("Enter value first")
+        
+    }
+    else{
+    for (var i = 0; i < cont.length; i++) {
+        if (cont[i] == "a" || cont[i] == "e" || cont[i] == "i" || cont[i] == "o" || cont[i] == "u") {
+            value = false;
+            break;
+        }
+    }
+    if (value == false) {
+        vowels.push(cont)
+        document.getElementById('a').innerHTML += "<li>" + cont + "</li>";
+    }
+    else {
+        value = true;
+        normals.push(cont)
+        document.getElementById('b').innerHTML += "<li>" + cont + "</li>";
+    }
+}
+}
